8610d7cf3a2698130e8db9ad74b77883e4eec7bd
her_username <robin@hifx.co.in>
2021-03-18 10:44:44 +0530

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,37 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  accountDetails = {
+  accountDetails:any = {
     1000: { acno: 1000, name: "userone", balance: 5000, password: "user1" },
     1001: { acno: 1001, name: "usertwo", balance: 3000, password: "user2" },
     1002: { acno: 1002, name: "userthree", balance: 4000, password: "user3" },
-    1003: { acno: 1003, name: "userfour", balance: 9000, password: "user4" },
-    1004: { acno: 1004, name: "userfive", balance: 9000, password: "user5" }
 }
+name = "Robin"
+accno = "";
+pwd = "";
+usrname = "";
 
-  uname= " ";
-  pwd= " ";
-  name = "Robin Joseph"
-  test = "Placeholder"
 
-  getuname(event:any){
-    this.uname=event.target.value;
-    // console.log(this.uname)
-  }
-  getpwd(event:any){
-    this.pwd=event.target.value;
-    // console.log(this.pwd)
-  }
+// getaccno(event:any){
+// 	this.accno = event.target.value;
+// }
+// getpwd(event:any){
+// 	this.pwd = event.target.value
+// }
 
-  constructor() { }
+
+  constructor(private routes:Router) { }
 
   ngOnInit(): void {
   }
+
   login(){
-    // var acno = a.value;
-    // var pwd = b.value;
-    alert("Login Works")
+	  var uname = this.usrname;
+	  var account_number = this.accno;
+	  var pass_word = this.pwd;
+	//   var account_number = a.value;
+	//   var pass_word = b.value;
+	  var datas = this.accountDetails;
+
+		if (account_number in datas){
+			var pwdstore = datas[account_number].password;
+			this.usrname = datas[account_number].name; // username
+			if(pwdstore == pass_word){
+				alert("Login Successfull");
+				this.routes.navigateByUrl("dashboard")
+			}
+			else{
+				alert("invalid Password");
+			}
+		}
+		else{
+			alert("Please enter a valid Account Number");
+		}
   }
+
 }
 
 
